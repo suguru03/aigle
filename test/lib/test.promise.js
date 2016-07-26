c6379259e4cd0545e7a8e3467fb3e841948b8c84
Suguru Motegi <suguru.motegi@gmail.com>
2016-07-26 12:29:57 -0700

    feat(promise): implement error handler

--- test/lib/test.promise.js
+++ test/lib/test.promise.js
@@ -15,14 +15,26 @@ parallel('#Promise', () => {
     })
     .then(value => {
       assert.strictEqual(value, 0);
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve(++value);
-        });
-      });
+      return ++value;
     })
     .then(value => {
       assert.strictEqual(value, 1);
+      return ++value;
+    });
+  });
+
+  it('should catch an error', () => {
+
+    var str = 'test';
+    return new Promise((resolve, reject) => {
+      reject(new Error('error'));
+    })
+    .catch(err => {
+      assert.ok(err);
+      return str;
+    })
+    .then(res => {
+      assert.strictEqual(res, str);
     });
   });
 });
