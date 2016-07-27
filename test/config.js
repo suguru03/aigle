9fd345930efba2cca88fcc51611b60d82c9e9541
Suguru Motegi <suguru.motegi@gmail.com>
2016-07-26 19:05:15 -0700

    feat(promise): implement error catcher

---
+++ test/config.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const DELAY = process.env.DELAY || 20;
+
+exports.DELAY = DELAY;
--- test/lib/test.promise.js
+++ test/lib/test.promise.js
@@ -1,40 +1,112 @@
 /* global it */
-
 'use strict';
 
 const assert = require('assert');
 
 const parallel = require('mocha.parallel');
-let Promise = require('../../');
+const Promise = require('../../');
+const DELAY = require('../config').DELAY;
 
 parallel('#Promise', () => {
 
-  it('should resolve on synchronous', () => {
-    return new Promise(resolve => {
+  it('should resolve on synchronous', done => {
+
+    let called = 0;
+    new Promise(resolve => {
       resolve(0);
+      ++called;
     })
     .then(value => {
       assert.strictEqual(value, 0);
+      ++called;
       return ++value;
     })
     .then(value => {
       assert.strictEqual(value, 1);
-      return ++value;
+      ++called;
     });
+    setTimeout(() => {
+      assert.strictEqual(called, 3);
+      done();
+    }, DELAY);
   });
 
-  it('should catch an error', () => {
+  it('should resolve on asynchronous', done => {
+
+    let called = 0;
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(0);
+        called++;
+      }, DELAY);
+    })
+    .then(value => {
+      assert.strictEqual(value, 0);
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(1);
+          called++;
+        }, DELAY);
+      });
+    })
+    .then(value => {
+      assert.strictEqual(value, 1);
+      called++;
+    });
+    setTimeout(() => {
+      assert.strictEqual(called, 3);
+      done();
+    }, DELAY * 3);
+  });
+
+  it('should catch an error', done => {
 
     var str = 'test';
-    return new Promise((resolve, reject) => {
+    var called = 0;
+    new Promise((resolve, reject) => {
       reject(new Error('error'));
+      called++;
     })
     .catch(err => {
       assert.ok(err);
+      called++;
       return str;
     })
     .then(res => {
       assert.strictEqual(res, str);
+      called++;
     });
+    setTimeout(() => {
+      assert.strictEqual(called, 3);
+      done();
+    }, DELAY);
+  });
+
+  it('should catch TypeError from error type', done => {
+
+    var str = 'test';
+    var called = 0;
+    new Promise((resolve, reject) => {
+      reject(new TypeError('error'));
+      called++;
+    })
+    .catch(ReferenceError, TypeError, err => {
+      assert.ok(err);
+      called++;
+      return str;
+    })
+    // should not be called this function
+    .catch(err => {
+      assert(!err);
+      called++;
+    })
+    .then(res => {
+      assert.strictEqual(res, str);
+      called++;
+    });
+    setTimeout(() => {
+      assert.strictEqual(called, 3);
+      done();
+    }, DELAY);
   });
 });
