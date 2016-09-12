32bc2f5f1942d4804d11343f211d3bf386294e22
Suguru Motegi <suguru.motegi@gmail.com>
2016-09-12 00:25:00 -0700

    feat(each): add each function

---
+++ test/lib/test.each.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+const Promise = require('../../');
+const util = require('../util');
+const DELAY = require('../config').DELAY;
+
+parallel('each', () => {
+
+  it('should execute on parallel', () => {
+
+    const order = [];
+    const delay = util.makeDelayTask(order);
+    const tasks = [
+      delay('test1', DELAY * 3),
+      delay('test2', DELAY * 2),
+      delay('test3', DELAY * 1)
+    ];
+    return Promise.each(tasks)
+      .then(res => {
+        assert.deepEqual(res, undefined);
+        assert.deepEqual(order, [
+          'test3',
+          'test2',
+          'test1'
+        ]);
+      });
+  });
+
+});
+
+parallel('#each', () => {
+
+  it('should execute on parallel', () => {
+
+    const order = [];
+    const delay = util.makeDelayTask(order);
+    const tasks = [
+      delay('test1', DELAY * 3),
+      delay('test2', DELAY * 2),
+      delay('test3', DELAY * 1)
+    ];
+    return Promise.resolve(tasks)
+      .each()
+      .then(res => {
+        assert.deepEqual(res, undefined);
+        assert.deepEqual(order, [
+          'test3',
+          'test2',
+          'test1'
+        ]);
+      });
+  });
+
+});
