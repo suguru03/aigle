e745168477f3ec4ed11eed76df5697ad2b54740e
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-03 22:27:30 -0800

    feat(filter): add filter

---
+++ test/lib/test.filter.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+const Aigle = require('../../');
+const DELAY = require('../config').DELAY;
+
+parallel('filter', () => {
+
+  it('should execute on parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.filter(collection, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+        assert.deepEqual(res, [1]);
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2],
+          [1, 4]
+        ]);
+      });
+  });
+
+  it('should execute with object collection on parallel', () => {
+
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.filter(collection, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Array]');
+        assert.deepEqual(res, [1]);
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2],
+          ['task2', 4]
+        ]);
+      });
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.filter(collection, iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
+parallel('#filter', () => {
+
+  it('should execute on parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .filter(iterator)
+      .then(res => {
+        assert.deepEqual(res, [1]);
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2],
+          [1, 4]
+        ]);
+      });
+  });
+
+  it('should execute with object collection on parallel', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .filter(iterator)
+      .then(res => {
+        assert.deepEqual(res, [1]);
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2],
+          ['task2', 4]
+        ]);
+      });
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.resolve(collection)
+      .filter(iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
