fb5efafdaf5f36c5c7255b972bd4f3fc413489d5
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-05 22:29:15 -0800

    feat(pickLimit): add pickLimit

---
+++ test/lib/test.pickLimit.js
@@ -0,0 +1,179 @@
+'use strict';
+
+const assert = require('assert');
+
+const _ = require('lodash');
+const parallel = require('mocha.parallel');
+const Aigle = require('../../');
+const DELAY = require('../config').DELAY;
+
+parallel('pickLimit', () => {
+
+  it('should execute', () => {
+
+    const order = [];
+    const collection = [1, 4, 2, 1];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.pickLimit(collection, 2, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          '0': 1,
+          '3': 1
+        });
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2],
+          [1, 4],
+          [3, 1]
+        ]);
+      });
+  });
+
+  it('should execute with object collection', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2,
+      task4: 1
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.pickLimit(collection, 2, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          'task1': 1,
+          'task4': 1
+        });
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2],
+          ['task2', 4],
+          ['task4', 1]
+        ]);
+      });
+  });
+
+  it('should execute with default concurrency which is 8', () => {
+
+    const collection = _.times(10);
+    const order = [];
+    const iterator = value => {
+      order.push(value);
+      return new Aigle(_.noop);
+    };
+    Aigle.pickLimit(collection, iterator);
+    return Aigle.delay(DELAY)
+      .then(() => {
+        assert.deepEqual(order, _.times(8));
+      });
+  });
+
+  it('should return an empty array if collection is an empty array', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.pickLimit([], iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {});
+      });
+  });
+
+  it('should return an empty array if collection is an empty object', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.pickLimit({}, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {});
+      });
+  });
+
+  it('should return an empty array if collection is string', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.pickLimit('test', iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {});
+      });
+  });
+});
+
+parallel('#pickLimit', () => {
+
+  it('should execute', () => {
+
+    const order = [];
+    const collection = [1, 4, 2, 1];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .pickLimit(2, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          '0': 1,
+          '3': 1
+        });
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2],
+          [1, 4],
+          [3, 1]
+        ]);
+      });
+  });
+
+  it('should execute with object collection', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2,
+      task4: 1
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .pickLimit(2, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          'task1': 1,
+          'task4': 1
+        });
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2],
+          ['task2', 4],
+          ['task4', 1]
+        ]);
+      });
+  });
+});
