ffcdc205a963ff24203d02c7d554fadff413c43b
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-05 22:24:16 -0800

    feat(pick): add pick

---
+++ test/lib/test.pick.js
@@ -0,0 +1,184 @@
+'use strict';
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+const Aigle = require('../../');
+const DELAY = require('../config').DELAY;
+
+parallel('pick', () => {
+
+  it('should execute on parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.pick(collection, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          '0': 1
+        });
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2],
+          [1, 4]
+        ]);
+      });
+  });
+
+  it('should execute with object collection on parallel', () => {
+
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.pick(collection, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          'task1': 1
+        });
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2],
+          ['task2', 4]
+        ]);
+      });
+  });
+
+  it('should return an empty array if collection is an empty array', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.pick([], iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {});
+      });
+  });
+
+  it('should return an empty array if collection is an empty object', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.pick({}, iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {});
+      });
+  });
+
+  it('should return an empty array if collection is string', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.pick('test', iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {});
+      });
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.pick(collection, iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
+parallel('#pick', () => {
+
+  it('should execute on parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .pick(iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          '0': 1
+        });
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2],
+          [1, 4]
+        ]);
+      });
+  });
+
+  it('should execute with object collection on parallel', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .pick(iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepEqual(res, {
+          'task1': 1
+        });
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2],
+          ['task2', 4]
+        ]);
+      });
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.resolve(collection)
+      .pick(iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
