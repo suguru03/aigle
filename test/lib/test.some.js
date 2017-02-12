c79449199435bb16af33b603b9a2c630e7c0f4ee
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-11 18:07:01 -0800

    feat(some): add some

---
+++ test/lib/test.some.js
@@ -0,0 +1,155 @@
+'use strict';
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+const Aigle = require('../../');
+const DELAY = require('../config').DELAY;
+
+parallel('some', () => {
+
+  it('should execute in parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.some(collection, iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          [0, 1]
+        ]);
+      });
+  });
+
+  it('should execute with object collection in parallel', () => {
+
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.some(collection, iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          ['task1', 1]
+        ]);
+      });
+  });
+
+  it('should return undefined if collection is an empty array', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.some([], iterator)
+      .then(res => assert.strictEqual(res, undefined));
+  });
+
+  it('should return undefined if collection is an empty object', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.some({}, iterator)
+      .then(res => assert.strictEqual(res, undefined));
+  });
+
+  it('should return undefined if collection is string', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.some('test', iterator)
+      .then(res => assert.strictEqual(res, undefined));
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.some(collection, iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
+parallel('#some', () => {
+
+  it('should execute in parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .some(iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          [0, 1]
+        ]);
+      });
+  });
+
+  it('should execute with object collection in parallel', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .some(iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          ['task1', 1]
+        ]);
+      });
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.resolve(collection)
+      .some(iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
