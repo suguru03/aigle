1fedc081d3f28cbbadf4b244f00dacb6497b593c
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-11 18:09:30 -0800

    feat(someLimit): add someLimit

---
+++ test/lib/test.someLimit.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const assert = require('assert');
+
+const _ = require('lodash');
+const parallel = require('mocha.parallel');
+const Aigle = require('../../');
+const DELAY = require('../config').DELAY;
+
+parallel('someLimit', () => {
+
+  it('should execute', () => {
+
+    const order = [];
+    const collection = [1, 4, 2, 1];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.someLimit(collection, 2, iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          [0, 1]
+        ]);
+      });
+  });
+
+  it('should execute with object collection', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2,
+      task4: 1
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.someLimit(collection, 2, iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          ['task1', 1]
+        ]);
+      });
+  });
+
+  it('should execute with default concurrency which is 8', () => {
+
+    const collection = _.times(10);
+    const order = [];
+    const iterator = value => {
+      order.push(value);
+      return new Aigle(_.noop);
+    };
+    Aigle.someLimit(collection, iterator);
+    return Aigle.delay(DELAY)
+      .then(() => {
+        assert.deepEqual(order, _.times(8));
+      });
+  });
+
+  it('should return an empty array if collection is an empty array', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.someLimit([], iterator)
+      .then(res => assert.strictEqual(res, undefined));
+  });
+
+  it('should return an empty array if collection is an empty object', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.someLimit({}, iterator)
+      .then(res => assert.strictEqual(res, undefined));
+  });
+
+  it('should return an empty array if collection is string', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.someLimit('test', iterator)
+      .then(res => assert.strictEqual(res, undefined));
+  });
+});
+
+parallel('#someLimit', () => {
+
+  it('should execute', () => {
+
+    const order = [];
+    const collection = [1, 4, 2, 1];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .someLimit(2, iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          [0, 1]
+        ]);
+      });
+  });
+
+  it('should execute with object collection', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2,
+      task4: 1
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .someLimit(2, iterator)
+      .then(res => {
+        assert.strictEqual(res, true);
+        assert.deepEqual(order, [
+          ['task1', 1]
+        ]);
+      });
+  });
+});
