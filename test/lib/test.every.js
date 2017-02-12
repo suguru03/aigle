61b3b14f45ae49ea53293419126558c8818d8b99
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-12 00:39:37 -0800

    feat(every): add every

---
+++ test/lib/test.every.js
@@ -0,0 +1,159 @@
+'use strict';
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+const Aigle = require('../../');
+const DELAY = require('../config').DELAY;
+
+parallel('every', () => {
+
+  it('should execute in parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.every(collection, iterator)
+      .then(res => {
+        assert.strictEqual(res, false);
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2]
+        ]);
+      });
+  });
+
+  it('should execute with object collection in parallel', () => {
+
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.every(collection, iterator)
+      .then(res => {
+        assert.strictEqual(res, false);
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2]
+        ]);
+      });
+  });
+
+  it('should return undefined if collection is an empty array', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.every([], iterator)
+      .then(res => assert.strictEqual(res, true));
+  });
+
+  it('should return undefined if collection is an empty object', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.every({}, iterator)
+      .then(res => assert.strictEqual(res, true));
+  });
+
+  it('should return undefined if collection is string', () => {
+
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.every('test', iterator)
+      .then(res => assert.strictEqual(res, true));
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.every(collection, iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
+parallel('#every', () => {
+
+  it('should execute in parallel', () => {
+
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .every(iterator)
+      .then(res => {
+        assert.strictEqual(res, false);
+        assert.deepEqual(order, [
+          [0, 1],
+          [2, 2]
+        ]);
+      });
+  });
+
+  it('should execute with object collection in parallel', () => {
+    const order = [];
+    const collection = {
+      task1: 1,
+      task2: 4,
+      task3: 2
+    };
+    const iterator = (value, key) => {
+      return new Aigle(resolve => setTimeout(() => {
+        order.push([key, value]);
+        resolve(value % 2);
+      }, DELAY * value));
+    };
+    return Aigle.resolve(collection)
+      .every(iterator)
+      .then(res => {
+        assert.strictEqual(res, false);
+        assert.deepEqual(order, [
+          ['task1', 1],
+          ['task3', 2]
+        ]);
+      });
+  });
+
+  it('should throw TypeError', () => {
+
+    const collection = [1, 4, 2];
+    const iterator = value => {
+      value.test();
+    };
+    return Aigle.resolve(collection)
+      .every(iterator)
+      .then(() => assert.ok(false))
+      .catch(TypeError, error => {
+        assert.ok(error);
+        assert.ok(error instanceof TypeError);
+      });
+  });
+});
+
