a73af682076854624b3a5654e981c609d5fbac50
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-13 12:34:34 -0800

    feat(whilst): add whilst

---
+++ test/lib/test.whilst.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const assert = require('assert');
+
+const parallel = require('mocha.parallel');
+const Aigle = require('../../');
+
+parallel('whilst', () => {
+
+  it('should execute', () => {
+
+    let count = 0;
+    const limit = 5;
+    const order = { test: [], iterator: [] };
+    const test = () => {
+      order.test.push(count);
+      return count < limit;
+    };
+    const iterator = () => {
+      order.iterator.push(count++);
+      return new Aigle(resolve => setImmediate(() => resolve(count)));
+    };
+    return Aigle.whilst(test, iterator)
+      .then(res => {
+        assert.deepEqual(order.iterator, [0, 1, 2, 3, 4]);
+        assert.deepEqual(order.test, [0, 1, 2, 3, 4, 5]);
+        assert.strictEqual(res, 5);
+      });
+  });
+
+  it('should execute with initial value', () => {
+
+    const value = 1;
+    const limit = 5;
+    const order = { test: [], iterator: [] };
+    const test = value => {
+      order.test.push(value);
+      return value < limit;
+    };
+    const iterator = value => {
+      order.iterator.push(value++);
+      return new Aigle(resolve => setImmediate(() => resolve(value)));
+    };
+    return Aigle.whilst(value, test, iterator)
+      .then(res => {
+        assert.deepEqual(order.iterator, [1, 2, 3, 4]);
+        assert.deepEqual(order.test, [1, 2, 3, 4, 5]);
+        assert.strictEqual(res, 5);
+      });
+  });
+
+  it('should execute with synchronous function', () => {
+
+    let sync = true;
+    let count = 0;
+    const test = str => str.length < 10;
+    const iterator = str => str + count++;
+    const promise = Aigle.whilst('num', test, iterator)
+      .then(res => {
+        assert.strictEqual(res, 'num0123456');
+        assert.strictEqual(sync, false);
+      });
+    sync = false;
+    return promise;
+  });
+});
+
+parallel('#whilst', () => {
+
+  it('should execute', () => {
+
+    const value = 1;
+    const limit = 5;
+    const order = { test: [], iterator: [] };
+    const test = value => {
+      order.test.push(value);
+      return value < limit;
+    };
+    const iterator = value => {
+      order.iterator.push(value++);
+      return new Aigle(resolve => setImmediate(() => resolve(value)));
+    };
+    return Aigle.resolve(value)
+      .whilst(test, iterator)
+      .then(res => {
+        assert.deepEqual(order.iterator, [1, 2, 3, 4]);
+        assert.deepEqual(order.test, [1, 2, 3, 4, 5]);
+        assert.strictEqual(res, 5);
+      });
+  });
+});
