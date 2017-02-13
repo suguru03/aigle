5d019c0a4185feba5a028b0a58340b85a1fc213f
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-13 13:45:19 -0800

    chore(test): disable mocha.parallel on circle ci

--- test/config.js
+++ test/config.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const path = require('path');
+
+if (process.env.CI) {
+  console.log('Disable mocha.parallel');
+  require('mocha.parallel');
+  const mochaParallelPath = path.resolve(__dirname, '..', 'node_modules', 'mocha.parallel', 'index.js');
+  require.cache[mochaParallelPath].exports = describe;
+}
+
 const DELAY = process.env.DELAY || 20;
 
 exports.DELAY = DELAY;
