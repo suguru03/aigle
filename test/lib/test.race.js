33467ce1249cb2d7792a404463b3dd3ddb1f54f2
Suguru Motegi <suguru.motegi@gmail.com>
2017-02-25 21:13:21 -0800

    feat(race): implement to allow object

--- test/lib/test.race.js
+++ test/lib/test.race.js
@@ -3,7 +3,7 @@
 const assert = require('assert');
 
 const parallel = require('mocha.parallel');
-const Promise = require('../../');
+const Aigle = require('../../');
 const util = require('../util');
 const DELAY = require('../config').DELAY;
 
@@ -18,7 +18,22 @@ parallel('race', () => {
       delay('test2', DELAY * 2),
       delay('test3', DELAY * 1)
     ];
-    return Promise.race(tasks)
+    return Aigle.race(tasks)
+      .then(res => {
+        assert.strictEqual(res, 'test3');
+        assert.deepEqual(order, ['test3']);
+      });
+  });
+
+  it('should execute with object tasks', () => {
+    const order = [];
+    const delay = util.makeDelayTask(order);
+    const tasks = {
+      task1: delay('test1', DELAY * 3),
+      task2: delay('test2', DELAY * 2),
+      task3: delay('test3', DELAY * 1)
+    };
+    return Aigle.race(tasks)
       .then(res => {
         assert.strictEqual(res, 'test3');
         assert.deepEqual(order, ['test3']);
@@ -34,7 +49,7 @@ parallel('race', () => {
       delay('test2', null, DELAY * 2),
       delay('test3', new Error('error3'), DELAY * 1)
     ];
-    return Promise.race(tasks)
+    return Aigle.race(tasks)
       .then(() => assert(false))
       .catch(err => {
         assert.ok(err);
@@ -43,6 +58,40 @@ parallel('race', () => {
       });
   });
 
+  it('should catch an error', () => {
+
+    const order = [];
+    const delay = util.makeDelayTask(order);
+    const tasks = {
+      task1: delay('test1', new Error('error1'), DELAY * 3),
+      task2: delay('test2', null, DELAY * 2),
+      task3: delay('test3', new Error('error3'), DELAY * 1)
+    };
+    return Aigle.race(tasks)
+      .then(() => assert(false))
+      .catch(err => {
+        assert.ok(err);
+        assert.strictEqual(err.message, 'error3');
+        assert.deepEqual(order, ['test3']);
+      });
+  });
+  it('should return undefined if tasks is an empty array', () => {
+
+    return Aigle.race([])
+      .then(res => assert.strictEqual(res, undefined));
+  });
+
+  it('should return undefined if tasks is an empty object', () => {
+
+    return Aigle.race({})
+      .then(res => assert.strictEqual(res, undefined));
+  });
+
+  it('should return undefined if tasks is empty', () => {
+
+    return Aigle.race()
+      .then(res => assert.strictEqual(res, undefined));
+  });
 });
 
 
@@ -57,7 +106,7 @@ parallel('#race', () => {
       delay('test2', DELAY * 2),
       delay('test3', DELAY * 1)
     ];
-    return Promise.resolve(tasks)
+    return Aigle.resolve(tasks)
       .race()
       .then(res => {
         assert.strictEqual(res, 'test3');
@@ -74,7 +123,7 @@ parallel('#race', () => {
       delay('test2', null, DELAY * 2),
       delay('test3', new Error('error3'), DELAY * 1)
     ];
-    return Promise.resolve(tasks)
+    return Aigle.resolve(tasks)
       .race()
       .then(() => assert(false))
       .catch(err => {
@@ -83,5 +132,4 @@ parallel('#race', () => {
         assert.deepEqual(order, ['test3']);
       });
   });
-
 });
