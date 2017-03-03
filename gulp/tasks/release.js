df13c45b1b51e607eb8a4b1a541497ca66ff8fab
Suguru Motegi <suguru.motegi@gmail.com>
2017-03-02 22:44:12 -0800

    chore(release): add release tasks [ci skip]

---
+++ gulp/tasks/release.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const path = require('path');
+
+const _ = require('lodash');
+const gulp = require('gulp');
+const git = require('gulp-git');
+const bump = require('gulp-bump');
+const runSequence = require('run-sequence');
+const tagVersion = require('gulp-tag-version');
+
+const packagepath = './package.json';
+const types = ['patch', 'minor', 'major'];
+
+_.forEach(types, type => {
+  gulp.task(`release:package:${type}`, updateVersion(type));
+  gulp.task(`release:${type}`, () => runSequence(
+    `release:package:${type}`,
+    'build',
+    'release:commit',
+    'release:tag'
+  ));
+});
+
+gulp.task('release:tag', () => {
+  return gulp.src(packagepath)
+    .pipe(tagVersion());
+});
+
+gulp.task('release:commit', () => {
+  const packagepath = path.resolve(__dirname, '../..', 'package.json');
+  delete require.cache[packagepath];
+  const { version } = require(packagepath);
+  return gulp.src(['./dist/*', packagepath])
+    .pipe(git.commit(version));
+});
+
+function updateVersion(type) {
+  return () => {
+    return gulp.src(packagepath)
+        .pipe(bump({ type }))
+        .pipe(gulp.dest('./'));
+  };
+}
