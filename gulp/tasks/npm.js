50b4346935171c487794a9fac1c2538d1f33dead
Suguru Motegi <suguru.motegi@gmail.com>
2017-08-02 10:06:22 -0700

    chore(npm): fix the publish task to avoid errors [ci skip]

--- gulp/tasks/npm.js
+++ gulp/tasks/npm.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 const gulp = require('gulp');
 const fs = require('fs-extra');
 
+const Aigle = require('../../');
 const { exec } = require('../util');
 
 gulp.task('npm:publish', publish);
@@ -14,26 +15,33 @@ async function publish() {
   const rootpath = path.resolve(__dirname, '../..');
   const buildpath = path.resolve(rootpath, 'build');
 
+  await exec(`rm -rf ${buildpath}`);
+
   // make dir
   !fs.existsSync(buildpath) && fs.mkdirSync(buildpath);
 
   // copy lib
-  const libpath = path.resolve(rootpath, 'lib');
-  fs.copySync(libpath, buildpath);
+  fs.copySync(path.resolve(rootpath, 'lib'), path.resolve(buildpath, 'lib'));
 
   // copy package.json
   const json = _.omit(require('../../package'), ['files', 'scripts']);
   json.main = 'aigle.js';
-  const filepath = path.resolve(buildpath, 'package.json');
-  fs.writeFileSync(filepath, JSON.stringify(json, null, 2), 'utf8');
+  fs.writeFileSync(path.resolve(buildpath, 'package.json'), JSON.stringify(json, null, 2), 'utf8');
 
   // copy README
   fs.copySync(path.resolve(rootpath, 'README.md'), path.resolve(buildpath, 'README.md'));
 
-  // rewrite util
-  const utilpath = path.resolve(buildpath, 'internal', 'util.js');
-  const file = fs.readFileSync(utilpath, 'utf8').replace('../../package.json', '../package.json');
-  fs.writeFileSync(utilpath, file, 'utf8');
+  // create all function files
+  const template = fs.readFileSync(path.resolve(__dirname, '../template'), 'utf8');
+  const aiglefile = template.replace(/require.*/, 'require(\'./lib/aigle\');');
+  fs.writeFileSync(path.resolve(buildpath, 'aigle.js'), aiglefile, 'utf8');
+  _.forOwn(Aigle, (func, key) => {
+    if (!_.isFunction(func) || /Error$/.test(key)) {
+      return;
+    }
+    const file = template.replace('<function>', key);
+    fs.writeFileSync(path.resolve(buildpath, `${key}.js`), file, 'utf8');
+  });
 
   await exec(`cd ${buildpath} && npm publish`);
 }
