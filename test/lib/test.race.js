a03c90498fb31c9ffa276e55c49acc1ec6d6e882
Suguru Motegi <suguru.motegi@gmail.com>
2017-12-26 11:28:19 +0900

    fix(race): fix race to work with a Map instance

--- test/lib/test.race.js
+++ test/lib/test.race.js
@@ -3,6 +3,7 @@
 const assert = require('assert');
 
 const parallel = require('mocha.parallel');
+
 const Aigle = require('../../');
 const util = require('../util');
 const { DELAY } = require('../config');
@@ -40,42 +41,20 @@ parallel('race', () => {
       });
   });
 
-  it('should catch an error', () => {
-
+  it('should execute with a Map instance', () => {
     const order = [];
     const delay = util.makeDelayTask(order);
-    const tasks = [
-      delay('test1', new Error('error1'), DELAY * 3),
-      delay('test2', null, DELAY * 2),
-      delay('test3', new Error('error3'), DELAY * 1)
-    ];
+    const tasks = new Map([
+      ['task1', delay('test1', DELAY * 3)],
+      ['task2', delay('test2', DELAY * 2)],
+      ['task3', delay('test3', DELAY * 1)]
+    ]);
     return Aigle.race(tasks)
-      .then(() => assert(false))
-      .catch(err => {
-        assert.ok(err);
-        assert.strictEqual(err.message, 'error3');
+      .then(res => {
+        assert.strictEqual(res, 'test3');
         assert.deepStrictEqual(order, ['test3']);
       });
   });
-
-  it('should catch an error', () => {
-
-    const order = [];
-    const delay = util.makeDelayTask(order);
-    const tasks = {
-      task1: delay('test1', new Error('error1'), DELAY * 3),
-      task2: delay('test2', null, DELAY * 2),
-      task3: delay('test3', new Error('error3'), DELAY * 1)
-    };
-    return Aigle.race(tasks)
-      .then(() => assert(false))
-      .catch(err => {
-        assert.ok(err);
-        assert.strictEqual(err.message, 'error3');
-        assert.deepStrictEqual(order, ['test3']);
-      });
-  });
-
   it('should be pending if tasks is an empty array', () => {
 
     let called = false;
@@ -120,6 +99,42 @@ parallel('race', () => {
     ])
     .then(res => assert.strictEqual(res, 1));
   });
+
+  it('should catch an error', () => {
+
+    const order = [];
+    const delay = util.makeDelayTask(order);
+    const tasks = [
+      delay('test1', new Error('error1'), DELAY * 3),
+      delay('test2', null, DELAY * 2),
+      delay('test3', new Error('error3'), DELAY * 1)
+    ];
+    return Aigle.race(tasks)
+      .then(() => assert(false))
+      .catch(err => {
+        assert.ok(err);
+        assert.strictEqual(err.message, 'error3');
+        assert.deepStrictEqual(order, ['test3']);
+      });
+  });
+
+  it('should catch an error', () => {
+
+    const order = [];
+    const delay = util.makeDelayTask(order);
+    const tasks = {
+      task1: delay('test1', new Error('error1'), DELAY * 3),
+      task2: delay('test2', null, DELAY * 2),
+      task3: delay('test3', new Error('error3'), DELAY * 1)
+    };
+    return Aigle.race(tasks)
+      .then(() => assert(false))
+      .catch(err => {
+        assert.ok(err);
+        assert.strictEqual(err.message, 'error3');
+        assert.deepStrictEqual(order, ['test3']);
+      });
+  });
 });
 
 
@@ -142,6 +157,34 @@ parallel('#race', () => {
       });
   });
 
+  it('should return first object if previous promise is already resolved', () => {
+
+    const order = [];
+    const delay = util.makeDelayTask(order);
+    const tasks = [
+      delay('test1', DELAY * 3),
+      delay('test2', DELAY * 2),
+      delay('test3', DELAY * 1)
+    ];
+    const promise = Aigle.resolve(tasks);
+    return Aigle.delay(DELAY * 4)
+      .then(() => promise.race())
+      .then(value => assert.strictEqual(value, 'test1'));
+  });
+
+
+  it('should work with pending proimse', () => {
+
+    const tasks = [
+      Aigle.delay(DELAY * 4, 1),
+      Aigle.delay(DELAY * 3, 2),
+      Aigle.delay(DELAY * 2, 3)
+    ];
+    return Aigle.delay(DELAY, tasks)
+      .race()
+      .then(res => assert.strictEqual(res, 3));
+  });
+
   it('should catch an error', () => {
 
     const order = [];
@@ -161,18 +204,4 @@ parallel('#race', () => {
       });
   });
 
-  it('should return first object if previous promise is already resolved', () => {
-
-    const order = [];
-    const delay = util.makeDelayTask(order);
-    const tasks = [
-      delay('test1', DELAY * 3),
-      delay('test2', DELAY * 2),
-      delay('test3', DELAY * 1)
-    ];
-    const promise = Aigle.resolve(tasks);
-    return Aigle.delay(DELAY * 4)
-      .then(() => promise.race())
-      .then(value => assert.strictEqual(value, 'test1'));
-  });
 });
