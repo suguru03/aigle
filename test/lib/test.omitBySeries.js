3e0b2e5d14a6fe5332bf6dfb0e7d85dd39971fff
Suguru Motegi <suguru.motegi@gmail.com>
2018-04-29 17:15:21 -0700

    fix(omit): fix omitSeries and omitLimit

--- test/lib/test.omitBySeries.js
+++ test/lib/test.omitBySeries.js
@@ -196,3 +196,28 @@ parallel('#omitBySeries', () => {
       });
   });
 });
+
+parallel('#omitSeries', () => {
+  it('should execute in series', () => {
+    const order = [];
+    const collection = [1, 4, 2];
+    const iterator = (value, key) => {
+      return new Aigle(resolve =>
+        setTimeout(() => {
+          order.push([key, value]);
+          resolve(value % 2);
+        }, DELAY * value)
+      );
+    };
+    return Aigle.resolve(collection)
+      .omitSeries(iterator)
+      .then(res => {
+        assert.strictEqual(Object.prototype.toString.call(res), '[object Object]');
+        assert.deepStrictEqual(res, {
+          '1': 4,
+          '2': 2
+        });
+        assert.deepStrictEqual(order, [[0, 1], [1, 4], [2, 2]]);
+      });
+  });
+});
