6b91a617ed5aa8b36ec1ccc4049a92181ed7a09a
Suguru Motegi <suguru.motegi@gmail.com>
2018-08-06 16:06:44 -0700

    chore(npm): update npm task for browser

--- gulp/tasks/npm.js
+++ gulp/tasks/npm.js
@@ -12,8 +12,8 @@ const { exec } = require('../util');
 gulp.task('npm:publish', publish);
 
 async function publish() {
-  const rootpath = path.resolve(__dirname, '../..');
-  const buildpath = path.resolve(rootpath, 'build');
+  const rootpath = path.join(__dirname, '../..');
+  const buildpath = path.join(rootpath, 'build');
 
   await exec(`rm -rf ${buildpath}`);
 
@@ -21,33 +21,37 @@ async function publish() {
   !fs.existsSync(buildpath) && fs.mkdirSync(buildpath);
 
   // copy lib
-  fs.copySync(path.resolve(rootpath, 'lib'), path.resolve(buildpath, 'lib'));
+  fs.copySync(path.join(rootpath, 'lib'), path.join(buildpath, 'lib'));
   // copy a minified file
-  fs.copySync(path.resolve(rootpath, 'dist', 'aigle-es5.min.js'), path.resolve(buildpath, 'aigle-es5.min.js'));
+  fs.copySync(path.join(rootpath, 'dist', 'aigle-es5.min.js'), path.join(buildpath, 'aigle-es5.min.js'));
 
   // copy package.json
   const json = _.omit(require('../../package'), ['files', 'scripts', 'private']);
-  json.main = 'aigle.js';
+  json.main = 'lib/aigle';
   json.browser = 'aigle-es5.min.js';
-  fs.writeFileSync(path.resolve(buildpath, 'package.json'), JSON.stringify(json, null, 2), 'utf8');
+  fs.writeFileSync(path.join(buildpath, 'package.json'), JSON.stringify(json, null, 2), 'utf8');
 
   // copy README
-  fs.copySync(path.resolve(rootpath, 'README.md'), path.resolve(buildpath, 'README.md'));
+  fs.copySync(path.join(rootpath, 'README.md'), path.join(buildpath, 'README.md'));
 
   // create all function files
-  const template = fs.readFileSync(path.resolve(__dirname, '../template'), 'utf8');
+  const template = `module.exports = require('./aigle')`;
   _.forOwn(Aigle, (func, key) => {
-    if (!_.isFunction(func) || /Error$/.test(key)) {
+    if (!_.isFunction(func) || /Error$|^Aigle$/.test(key)) {
       return;
     }
-    const file = template.replace('<function>', key);
-    fs.writeFileSync(path.resolve(buildpath, `${_.camelCase(key)}.js`), file, 'utf8');
+    const file = `${template}.${key};`;
+    fs.writeFileSync(path.join(buildpath, `${_.camelCase(key)}.js`), file, 'utf8');
   });
-  const aiglefile = template.replace(/require.*/, "require('./lib/aigle');");
-  fs.writeFileSync(path.resolve(buildpath, 'aigle.js'), aiglefile, 'utf8');
+
+  // craete aigle files
+  const aigledirpath = path.join(buildpath, 'aigle');
+  const pkg = { main: '../lib/aigle', browser: '../aigle-es5.min.js' };
+  fs.mkdirSync(aigledirpath);
+  fs.writeFileSync(path.join(aigledirpath, 'package.json'), JSON.stringify(pkg), 'utf8');
 
   // copy type files
-  fs.copySync(path.resolve(rootpath, 'typings', 'aigle.d.ts'), path.resolve(buildpath, 'aigle.d.ts'));
+  fs.copySync(path.join(rootpath, 'typings', 'aigle.d.ts'), path.join(buildpath, 'aigle.d.ts'));
 
   // TODO: fix publish task
   // const tag = /alpha|beta/.test(json.version) ? '--tag next' : '';
