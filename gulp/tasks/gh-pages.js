df9f551ee7e3bd2d6a28cc0e445e6fd188c21fb4
Suguru Motegi <suguru.motegi@gmail.com>
2019-01-13 19:03:39 -0800

    chore(gulp): fix gh-pages tasks

--- gulp/tasks/gh-pages.js
+++ gulp/tasks/gh-pages.js
@@ -13,23 +13,30 @@ Aigle.promisifyAll(git);
 
 let version;
 
-gulp.task('gh-pages:version', () => {
+gulp.task('gh-pages:version', async () => {
   const packagepath = path.resolve(__dirname, '../..', 'package.json');
   delete require.cache[packagepath];
   version = require(packagepath).version;
 });
 
-gulp.task('gh-pages:add', () => {
-  return gulp.src('./docs/*').pipe(git.add({ args: '-f' }));
-});
+gulp.task('gh-pages:add', () => gulp.src('./docs/*').pipe(git.add({ args: '-f' })));
+
+gulp.task('gh-pages:reset', () => git.reset('HEAD'));
 
 gulp.task('gh-pages:stash', git.stash);
 
-gulp.task('gh-pages:pop', () => git.execAsync({ args: 'checkout stash -- .' }));
+gulp.task('gh-pages:stash:drop', cb => git.stash({ args: 'drop' }, cb));
 
-gulp.task('gh-pages:commit', () => {
-  return gulp.src('./docs/*').pipe(git.commit(`docs(jsdoc): v${version} [ci skip]`));
-});
+gulp.task('gh-pages:pop', cb => git.exec({ args: 'checkout stash -- .' }, cb));
+
+gulp.task('gh-pages:commit', () =>
+  gulp.src('./docs/*').pipe(
+    git.commit(`docs(jsdoc): v${version} [ci skip]`, {
+      args: '--no-verify',
+      disableAppendPaths: true
+    })
+  )
+);
 
 gulp.task(
   'gh-pages',
@@ -40,7 +47,10 @@ gulp.task(
     'gh-pages:stash',
     'checkout:gh-pages',
     'gh-pages:pop',
+    'gh-pages:reset',
+    'gh-pages:add',
     'gh-pages:commit',
-    'checkout:master'
+    'checkout:master',
+    'gh-pages:stash:drop'
   )
 );
