bf33123b04ce7f0a74a63bf9e852b18831d7e075
Adrian <agierakowski@gmail.com>
2020-04-13 18:32:17 +0100

    retry: add predicate option (currently only function) (#123)
    Predicate function is executed with rejection value, if the call returns
    a truthy value then the handler is retried, otherwise the promise is
    rejected with the original rejection value

--- typings/aigle.d.ts
+++ typings/aigle.d.ts
@@ -4587,6 +4587,7 @@ declare namespace Aigle {
   export interface RetryOpts {
     times?: number;
     interval?: number | ((count?: number) => number);
+    predicate?: (error?: any) => boolean;
   }
 
   export class Disposer<T> {}
