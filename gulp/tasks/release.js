0f1976142d04376dbd904164dc06c329c406446b
Suguru Motegi <suguru.motegi@gmail.com>
2020-04-15 20:59:09 -0700

    chore(release): remove fs-extra

--- gulp/tasks/release.js
+++ gulp/tasks/release.js
@@ -1,10 +1,10 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
 
 const _ = require('lodash');
 const gulp = require('gulp');
-const fs = require('fs-extra');
 const git = require('gulp-git');
 const semver = require('semver');
 const bump = require('gulp-bump');
@@ -72,12 +72,14 @@ async function createPackage() {
   await exec(`rm -rf ${buildpath}`);
 
   // make dir
-  !fs.existsSync(buildpath) && fs.mkdirSync(buildpath);
+  if (!fs.existsSync(buildpath)) {
+    fs.mkdirSync(buildpath);
+  }
 
   // copy lib
-  fs.copySync(path.join(rootpath, 'lib'), path.join(buildpath, 'lib'));
+  fs.copyFileSync(path.join(rootpath, 'lib'), path.join(buildpath, 'lib'));
   // copy a minified file
-  fs.copySync(
+  fs.copyFileSync(
     path.join(rootpath, 'dist', 'aigle-es5.min.js'),
     path.join(buildpath, 'aigle-es5.min.js')
   );
@@ -89,7 +91,7 @@ async function createPackage() {
   fs.writeFileSync(path.join(buildpath, 'package.json'), JSON.stringify(json, null, 2), 'utf8');
 
   // copy README
-  fs.copySync(path.join(rootpath, 'README.md'), path.join(buildpath, 'README.md'));
+  fs.copyFileSync(path.join(rootpath, 'README.md'), path.join(buildpath, 'README.md'));
 
   // create all function files
   const template = `module.exports = require('./aigle')`;
@@ -108,5 +110,5 @@ async function createPackage() {
   fs.writeFileSync(path.join(aigledirpath, 'package.json'), JSON.stringify(pkg), 'utf8');
 
   // copy type files
-  fs.copySync(path.join(rootpath, 'typings', 'aigle.d.ts'), path.join(buildpath, 'aigle.d.ts'));
+  fs.copyFileSync(path.join(rootpath, 'typings', 'aigle.d.ts'), path.join(buildpath, 'aigle.d.ts'));
 }
